663a5601894194862fd34c1fc1e58484685a5fd6
coltynw <coltynw@users.github.com>
2020-05-22 13:52:22 -0400

    finished in time, with tons of comments :)

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,71 @@
 
 ## Exercise I
 
-a)
+a)  
+<!-- a = 0
+while (a < n * n * n):
+    a = a + n * n -->
+
+    n reffered to the size of the input, but here n is an input. 
+    it's only a single input, it's just the one n. 
+    but if that n increases it will multiplicatively affect the runtime because we're multiplying it by itself like 5 times.
+    They picked a variable named n to make it more confusing.
+
+    it's O(n)
 
 
 b)
+<!-- sum = 0
+for i in range(n):
+      j = 1
+      while j < n:
+        j *= 2
+        sum += 1 -->
 
+    ok this is 2 loops, a for loop and a while loop. 4 total variables but it looks like only 2 of them are inputs. 
+    The first loops runs n number of times, so the length of n is how long we are iterating through n. the while loop inside we run that many times at least as long as j is less than n. Since we're multiplying inputs by each other It'd say this is slightly less effiecent than the a) question. So I'm just going to guess because none of the other runtime expressions fit this discription.
+
+    it's O(n log n)?
 
 c)
+ <!-- def bunnyEars(bunnies):
+     if bunnies == 0:
+        return 0
+
+      return 2 + bunnyEars(bunnies-1) -->
+
+      recursive function. Looks like only 1 input tho, == bunnies. This function runs (bunnies) number of times. That means it's Linear
+
+      it's O(n)
 
 ## Exercise II
+ <!-- Suppose that you have an n-story building and plenty of eggs. Suppose also that an egg gets broken if it is thrown off floor f or higher, and doesn't get broken if dropped off a floor less than floor f. Devise a strategy to determine the value of f such that the number of dropped + broken eggs is minimized.
+
+ Write out your proposed algorithm in plain English or pseudocode AND give the runtime complexity of your solution. -->
 
 
+This is simply going to have to be a loop or use recursive of some kind, that rules out the first 3, constant, logarithmic and linear.
+
+Since we can sort large sums of the data (the floors) at once by checking if the egg breaks, recursive sorting and iterative sorting doesn't really seem correct so I'm going to review my notes.
+
+This is a sorted data set, the floors never change in order, so I'm going to think Binary Search. I'm going to look up more on Binary Search since that was like Monday and I don't really remember it.
+
+Binary Search looks similar to a quick sort setting pivots, but the pivots are always the middle of the current unsorted data.
+
+It's assumed you start at the bottom floor?
+I'm just gunna say we start in the middle.
+    
+    So go to the middle floor.
+        drop the egg.
+            if the egg breaks
+                go to middle floor between you and the ground or the lowest floor that an egg has previously broken on.
+            if the egg does not break
+                go to the middle floor between you and the roof or the highest floor that an egg has not previously broken on.
+
+    keep repeating these methods in the middle of the middles until you get to the highest floor the egg doesn't break on.
+        
+    set that floor to f
+
+    print(f)
+        
+google says Binary Search is O(log n)
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,55 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        
+        # understand
+        # this felt really complicated at first but we just have to sort this output somehow using the functions that are already aviable to me that are defined earlier in this robot class.
+
+        # the buttom 3 methods seem useless, I can't find in the instructions why the light would want to be on? And the word 'light' is not anywhere in the test file, I can just ignore these.
+
+        # I thought the top 2 method's were useless as well, as trying to move and checking to move both return False, so why would u ever check?
+        # But then I realized I can turn the light on and off to signal to other functions to run or not run to help sort the nodes. And checking to see if you can move left or right, just checks if you are at the start or end of the array without having to try to move.
+
+        # plan
+        # I'm not going to assume we're lucky enough to start at the beginning of the array, even though thats the most likely scenario, we have no way of knowing our location, so i'm starting with the assumption we are in the middle of the array, and not holding an item. 
+        # I'm going to use 3 while loops, the first one to turn the light on. The seconed one to run if we have a node that is greater than the last node and unsorted. And the third loop to do the same thing but with a lesser node. The 2nd and 3rd while loops check to see if the light from the first while loop is on before they run their functions, and turn the light off when they are finished so we know they have ran.
+
+        # exicute
+        while self.light_is_on() == False: # if the lights off, turn it on so we know we are NOT currently holding an unsorted node and we need the rest of the methods in this while loop to be performed.
+            self.set_light_on() 
+            
+            while self.can_move_right() == True: # check if ur not at the end of the array. 
+                self.swap_item() # if you are not, swap your item to pick one up. We don't want to pick up the last item in the array, so this makes sure we don't do that. And if this is the 2nd time this function is ran, we are picking up the bigger number.
+                self.move_right() # and then actually move right while holding your item. If this is not the first time this is ran, then we know we are holding a number bigger than the previous number.
+
+                # so lets check if its bigger than the new number in front of us since we just moved right.
+                if self.compare_item() == 1: # if the compare item function returns 1, that means the item in our hand is greater than the item in front of us.
+                    self.swap_item() # pick up the smaller node
+                    self.set_light_off() # turn the light off to show that we are currently in the process of sorting, we are holding an unsorted node and other functions should not be ran until this number is in the next best position. This also restarts the while loop. And at the start, we will do a swap, so that will pick up the greater number that we just set down and then move it to the right, and then recompare it to the next number.
+
+                # if self.compare_item() != 1 then the item in our hands is less than the one in front of us. I would specify this in code with an if statement, but we don't really need to because the while loop takes care of it, and if self.compare_item() == 0 then it doesn't matter what we do because the numbers are equal anyway.    
+                self.move_left() # so lets go left since thats were the smaller numbers go
+                self.swap_item() # then swap the items to set down the smaller number and pick up a new number
+                self.move_right() # the move right so when the while loop restarts we can compare again.
+
+                # so the goal of this first loop to always put down the greater node and always pick up the lesser node, but at the start, do a swap and move right, so the greater node is always moving right.
+            
+            # the 2nd loop is going to the do the same but to the right, and it will run if the light is still on, which means self.compare_item() returned less than 1. Which means we are holding a number that is currently greater than the one in front of us.
+            while self.can_move_left(): # if you can move left
+                self.swap_item() # put down the old item and pick up a new one.
+                self.move_left() # and then actually move left
+               
+                if self.compare_item() == -1: # if the compare item function returns -1 that means the item in our hand is greater than the one in front of us.
+                    self.swap_item() # pick up the greater node.
+                    self.set_light_off() # turn the light off to show that we are currently in the process of sorting, we are holding an unsorted node and other functions should not be ran until this number is in the next best position. This also restarts the while loop.
+                    
+                self.move_right() # go back to the right
+                self.swap_item() # set down the item we just got information about and pick up a new one.
+                self.move_left() # go back to the left again, since the start of the while loop moves you right if you are able to, so we need to cancel that out when the loop restarts.
+                
+
+    # review 
+    # I know there was probably a much easier way to do this and my head hurts, I spent most of the time during the sprint on this, but the only real hard part was not thinking about it so linearly. Since I started the while loop with checking and moving to the right, the rest of the code reflects and expects that, which was difficult to properly think through. 
 
 
 if __name__ == "__main__":
